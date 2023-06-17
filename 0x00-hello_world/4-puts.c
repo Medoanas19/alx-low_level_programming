ce004d6c3616b1e9680fcfa762cf2029b7142baf
Medoanas19 <Medanas.choumoul@gmail.com>
2023-06-16 21:33:50 -0700

    a C program that prints exactly that prints out a statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the first line in C that prints a statement
+ * Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
