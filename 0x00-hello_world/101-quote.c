1d87dab400ae9bef4721ac33458071621a65582c
Medoanas19 <Medanas.choumoul@gmail.com>
2023-06-16 22:54:44 -0700

    A C statement without printf or put

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Printing a statement without using printf or puts
+ * Return:1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
