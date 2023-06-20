ddf78eef9885076736675b471ee6023ce28ce147
Medoanas19 <Medanas.choumoul@gmail.com>
2023-06-19 17:31:01 -0700

    prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,16 +5,12 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int num;
+
+	for (num = 68; num < 78; num++)
+	{
+		putchar(num);
+	}
 	putchar('\n');
 
 	return (0);
